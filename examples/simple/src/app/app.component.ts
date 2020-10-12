6f1a08091c7f0b9c4928b5d34ff9fac0a82bb013
Marc-Antoine Ferland <maferland@hey.com>
2020-10-11 23:55:33 -0400

    feat: link angular-query to simple example

--- examples/simple/src/app/app.component.ts
+++ examples/simple/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AngularQueryService } from 'angular-query';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'simple';
+
+  private subscription: Subscription;
+
+  constructor(private angularQueryService: AngularQueryService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.angularQueryService.useQuery().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
